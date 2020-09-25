60dfb162214f36696f1eef679f18e7717c5a8f2e
edison.zuo <1940775885@qq.com>
2020-09-25 11:05:40 +0800

    新增zookeeper配置

--- sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/zookeeper/ZookeeperConfig.java
+++ sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/rule/zookeeper/ZookeeperConfig.java
@@ -20,6 +20,7 @@ import java.util.List;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.ExponentialBackoffRetry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -32,6 +33,9 @@ import com.alibaba.fastjson.JSON;
 @Configuration
 public class ZookeeperConfig {
 
+	@Value("${zookeeper.cluster:127.0.0.1:2181}")
+	private String zookeeperUrl;
+	
     @Bean
     public Converter<String, List<FlowRuleEntity>> flowRuleEntityDecoder() {
         return s -> JSON.parseArray(s, FlowRuleEntity.class);
@@ -65,7 +69,7 @@ public class ZookeeperConfig {
     @Bean
     public CuratorFramework zkClient() {
         CuratorFramework zkClient =
-                CuratorFrameworkFactory.newClient("127.0.0.1:2181",
+                CuratorFrameworkFactory.newClient(zookeeperUrl,
                         new ExponentialBackoffRetry(ZookeeperConfigUtil.SLEEP_TIME, ZookeeperConfigUtil.RETRY_TIMES));
         zkClient.start();
 
